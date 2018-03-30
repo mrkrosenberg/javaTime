bfecf5464e73010a4f87d3503d5ea01917a6fc5a
Mark Rosenberg <mrk.rosenberg@gmail.com>
2018-03-30 11:41:18 -0600

    class/global variables and local variables

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,19 +4,30 @@ import java.util.Scanner;
 // blue print for our program
 public class HelloWorld {
 	
-	
-	static Scanner userInput = new Scanner(System.in);
+	static double myPI = 3.14149; // class variable - accessible from other classes
 	
 	public static void main(String[] args) {
 		
-		int randomNumber = (int) (Math.random() * 50);
+//		addThem(1, 2);
+		System.out.println(addThem(1, 2));
 		
-		if (randomNumber < 25) {
-			System.out.println("The random number is less than 25");
-		}
-		System.out.println("The random number is " + randomNumber);
-			
+		
 	}
 	
+	
+	public static int addThem(int a, int b) {
+		
+		double smallPI = 3.14; // local variable  - only available in this method
+		
+//		double myPI = 3.0;
+//		myPI = myPI * 3;
+//		
+//		System.out.println("Local PI " + myPI);
+		
+		int c = a + b;
+		
+		return c;
+		
+	}
 	
 }
